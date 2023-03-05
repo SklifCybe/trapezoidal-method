9e3e608c1b9105c517937af97d55c7c76dd5a416
Ilya Strelkovskiy <IStrelkovskiy@alfabank.ru>
2023-03-05 23:05:31 +0300

    create some types

---
+++ src/types/integral-variables.ts
@@ -0,0 +1,7 @@
+export type IntegralVariables = {
+    startBorder: number;
+    endBorder: number;
+    countOfIntegral: number;
+    functionByX: string;
+    rounding: number;
+};
---
+++ src/types/questions-and-answers.ts
@@ -0,0 +1,6 @@
+import { IntegralVariables } from './integral-variables.js';
+
+export type QuestionsAndAnswers = {
+    variables: Partial<IntegralVariables>;
+    allExample: string;
+};
