33d5489e1cbfde2c966050ed394cebe7da0fb841
Ilya Strelkovskiy <IStrelkovskiy@alfabank.ru>
2023-03-05 23:04:54 +0300

    add data files

---
+++ src/data/math-answer-keys.ts
@@ -0,0 +1,9 @@
+import type { IntegralVariables } from '../types/integral-variables.js';
+
+export const mathAnswerKeys: Array<keyof IntegralVariables> = [
+    'startBorder',
+    'endBorder',
+    'countOfIntegral',
+    'functionByX',
+    'rounding',
+];
